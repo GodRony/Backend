adda56e59fb7d3e6d3f7139fb9658b6840dfdad4
GodRony <gmldud593@gmail.com>
2025-03-10 21:44:41 +0900

    Add files via upload

---
+++ SPRING_UDEMY/game/gameExecute/App03GamingSpringBeans.java
@@ -0,0 +1,14 @@
+package gameExecute;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App03GamingSpringBeans {
+    public static void main(String[] args){
+        var  context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+
+        context.getBean(GamingConsole.class).up();
+        context.getBean(GameRunner.class).run();
+    }
+}
