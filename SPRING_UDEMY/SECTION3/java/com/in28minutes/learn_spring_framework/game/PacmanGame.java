4453fe5ad0d75336464eb540708b94325ae90acd
GodRony <gmldud593@gmail.com>
2025-03-12 20:26:07 +0900

    Add files via upload

---
+++ SPRING_UDEMY/SECTION3/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learn_spring_framework.game;
+
+public class PacmanGame implements GamingConsole{
+    @Override
+    public void up() {
+        System.out.println("p UP");
+    }
+    @Override
+    public void down() {
+        System.out.println("p Down");
+
+    }
+
+    @Override
+    public void left() {
+        System.out.println("p Left");
+
+    }
+
+    @Override
+    public void right() {
+        System.out.println("p Right");
+    }
+}
